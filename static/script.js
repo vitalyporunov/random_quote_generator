4c93c36a4e317af78a5c73db2b091c1f82911e8c
Vitaly Porunov <73434160+vitalyporunov@users.noreply.github.com>
2024-09-27 17:26:41 +0100

    added HTML and CSS changes for usability

--- static/script.js
+++ static/script.js
@@ -4,3 +4,11 @@ console.log('JavaScript Loaded');
 function notifyFavorite() {
     alert('Quote has been added to your favorites!');
 }
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.getElementById('toggle-dark-mode');
+
+    toggleButton.addEventListener('click', function() {
+        document.body.classList.toggle('dark-mode');
+        toggleButton.textContent = document.body.classList.contains('dark-mode') ? '🌞' : '🌙';
+    });
+});
