dbfea5f34ca4ef201d41051fb9145456ef788128
Vitaly Porunov <73434160+vitalyporunov@users.noreply.github.com>
2024-09-27 17:07:39 +0100

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,6 @@
+// script.js - Future JavaScript for interactivity
+console.log('JavaScript Loaded');
+// Notify when a quote is favorited
+function notifyFavorite() {
+    alert('Quote has been added to your favorites!');
+}
